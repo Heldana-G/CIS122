7459c85538ab3d87a7566d6ffc006d3869165306
Heldana-G <heldana.deyaso@mnsu.edu>
2025-05-04 14:32:54 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Solution
+{
+    public bool IsHappy(int n)
+    {
+        HashSet<int> seen = new HashSet<int>();
+
+        while (n != 1 && !seen.Contains(n))
+        {
+            seen.Add(n);
+            n = SumOfSquares(n);
+        }
+
+        return n == 1;
+    }
+
+    private int SumOfSquares(int num)
+    {
+        int sum = 0;
+        while (num > 0)
+        {
+            int digit = num % 10;
+            sum += digit * digit;
+            num /= 10;
+        }
+        return sum;
+    }
+}
